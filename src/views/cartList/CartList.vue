5cb83715d90d6ef185dfde32709725e23ec125c7
natsuki <362585052@qq.com>
2021-10-29 22:19:00 +0800

    add

---
+++ src/views/cartList/CartList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cart">CartList</div>
+</template>
+
+<script>
+export default {
+  name: 'CartList'
+}
+</script>
+
+<style lang="scss" scoped>
+.cart {
+  margin-top: .2rem;
+  text-align: center;
+}
+</style>
